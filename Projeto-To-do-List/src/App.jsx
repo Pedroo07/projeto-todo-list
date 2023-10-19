49b935283a6f0cb22b351c354bb6d882d4db632d
Pedroo07 <123650104+Pedroo07@users.noreply.github.com>
2023-10-19 14:12:08 -0300

    enviando projeto para deploy

---
+++ Projeto-To-do-List/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+import Search from './components/Search'
+import Filter from './components/Filter'
+
+function App() {
+  const [todos, setTodos] = useState(
+    [
+      {
+        id: 1,
+        text: "Fazer Lista De Tarefas",
+        category: "Estudos",
+        isCompleted: false,
+      },
+    ]
+  )
+  const [search, setSearch] = useState('')
+  const [filter, setFilter] = useState('All')
+  const [sort, setSort] = useState('Asc')
+  const addTodo = (text, category) => {
+
+    const newTodo = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }]
+    setTodos(newTodo)
+  }
+
+  const removeTodo = (id) => {
+    const newTodo = [...todos]
+    const filterTodo = newTodo.filter((todo) => todo.id !== id ? todo : null)
+    setTodos(filterTodo)
+  }
+  const completeTodo = (id) => {
+    const newTodo = [...todos]
+    newTodo.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodo)
+  }
+  return (
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      <div className="todo-list">
+        {todos.filter((todo) =>
+          filter === "All" ? true : filter === "completed" ? todo.isCompleted : !todo.isCompleted
+        ).filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)).map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  )
+}
+
+export default App
